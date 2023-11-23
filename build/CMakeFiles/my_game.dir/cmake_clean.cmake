a9c48fdc83a9fcddc811efda7d79198193068200
00000000014B1261 <salih.uyar@tum.de>
2023-11-23 16:52:45 +0100

    basic Cmake

---
+++ build/CMakeFiles/my_game.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_game.dir/game.cpp.o"
+  "CMakeFiles/my_game.dir/game.cpp.o.d"
+  "my_game"
+  "my_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
